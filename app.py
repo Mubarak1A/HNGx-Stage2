a933a1df46d9973f3592eade4f8043fd447f1ced
Mubarak1A <adesinamubarak123@example.com>
2023-09-13 11:57:11 +0100

    CREATE route done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """A simple REST API app capable of CRUD operations."""
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,10 +18,28 @@ class Person(db.Model):
         '''initialize class varible name'''
         self.name = name
 
+#create db tables
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 def index():
     """Home page route function"""
     return '<h1> HNGx-Stage 2 CRUD project task! </h1>'
+
+@app.route('/api/person', methods=['POST'])
+def create_user():
+    '''Add the user to the database'''
+    name = request.json.get('name')
+
+    if not name:
+        return jsonify({'message': 'name is required'}), 404
+
+    person = Person(name=name)
+    db.session.add(person)
+    db.session.commit()
+
+    return jsonify({'message': 'Person created Successfully'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
